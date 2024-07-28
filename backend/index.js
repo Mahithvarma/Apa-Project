e72f245c2d67e4a8dd48f56eea25d2e064a4ad8d
JAMPANA MAHEETH SAI DURGA VARMA <21131a0581@gvpce.ac.in>
2024-07-28 15:48:33 +0530

    database is created and setup completely

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+
 const app = express();
-const port = 3000;
+require('dotenv').config();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
 
-// Define a simple route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+const PORT = process.env.PORT;
+const mongo_url = process.env.MONGO_URL;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Failed to connect to MongoDB:', error);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}); 
